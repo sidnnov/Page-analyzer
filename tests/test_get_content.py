b4e489606f5627b9a1f4937a1e372d70ae20b676
sidnnov <sidnnov@gmail.com>
2023-03-21 15:31:26 +0100

    Fix test_get_content.py

--- tests/test_get_content.py
+++ tests/test_get_content.py
@@ -7,20 +7,16 @@ URL_3 = "tests/fixtures/url_3.html"
 URL_4 = "tests/fixtures/url_4.html"
 
 
-def read_html(url):
-    with open(url) as file:
-        meta = file.read()
-    return meta
-
-
 @pytest.mark.parametrize(
     "url, result",
     [
-        (read_html(URL_1), ("h1", "title", "description")),
-        (read_html(URL_2), ("", "title", "description")),
-        (read_html(URL_3), ("h1", "", "description")),
-        (read_html(URL_4), ("h1", "title", "")),
+        (URL_1, ("h1", "title", "description")),
+        (URL_2, ("", "title", "description")),
+        (URL_3, ("h1", "", "description")),
+        (URL_4, ("h1", "title", "")),
     ],
 )
 def test_get_content(url, result):
-    assert get_content(url) == result
+    with open(url) as file:
+        meta_str = file.read()
+    assert get_content(meta_str) == result
