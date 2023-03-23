c2d3753d3da5bc91157eb829da4c8750fe15b3ff
sidnnov <sidnnov@gmail.com>
2023-03-23 12:29:59 +0100

    Add test_get_data_from_url.py

---
+++ tests/test_get_data_from_url.py
@@ -0,0 +1,20 @@
+from page_analyzer.utility import get_data_from_url
+import responses
+
+
+@responses.activate
+def test_get_data_from_url():
+    url = 'https://example.com/'
+    expected_status_code = 200
+    expected_text = 'Some text'
+    responses.add(
+        responses.GET,
+        url,
+        status=expected_status_code,
+        body=expected_text
+    )
+    assert expected_status_code, expected_text == get_data_from_url(url)
+
+    responses.reset()
+    responses.add(responses.GET, url, status=404)
+    assert get_data_from_url(url) is None
